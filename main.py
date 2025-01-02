ba3ef08e73a7fd92f4ac4d04750ec0f550c46273
Mark Peters <mapeters101@earthlink.net>
2025-01-02 18:22:01 -0500

    Enter conversion functions.

---
+++ main.py
@@ -0,0 +1,22 @@
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) +32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def celsius_to_kelvin(celsius):
+    return celsius + 273.15
+
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
+
+def fahrenheit_to_kelvin(fahrenheit):
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    return celsius_to_kelvin(celsius)
+
+def kelvin_to_fahrenheit(kelvin):
+    celsius = kelvin_to_celsius(kelvin)
+    return celsius_to_fahrenheit(celsius)
+
+
